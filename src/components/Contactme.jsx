bd6d3471592007700a4d2194a063b1d3339bd42f
sirilca <sirilcantony@gmail.com>
2023-11-08 13:02:04 +0530

    form added for contact info

--- src/components/Contactme.jsx
+++ src/components/Contactme.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import emailjs from 'emailjs-com'
 import './Contactme.css'
 function Contactme() {
 
@@ -10,10 +11,19 @@ function Contactme() {
   const [isvalid, setIsvalid] = useState(false)
   const[testmail,setTestmail]=useState(true)
   // const[data,setData] = useState([])
-
-  const submitform = () => {
+  const formRef = useRef()
+  const submitform = (e) => {
+    e.preventDefault()
     setTestmail(isvalid)
+    if(isvalid){
+      console.log('validd')
+      emailjs.sendForm('service_qup782i', 'template_ryvb5pb', formRef.current, 'XeRBFKh5BwdgB76jJ')
+      .then(res=>console.log(res))
+      .catch(err=>console.log("error occured",err))
+      nav('/')
+    }
   }
+
   
   
   const mailverify = (e) => {
@@ -44,41 +54,40 @@ function Contactme() {
         <h1>Thanks for taking the time to reach out. How can I help you today?</h1>
       </div>
 
+      
+      <form ref={formRef} onSubmit={submitform}>
+        <div className='inputentry'>
+          <div>
+            <div className='emname'>
+              <div className='name'>
+                <label>Name</label>
+                <input type='text' name='from_name' value={name} onChange={(e) => setName(e.target.value)} />
+              </div>
 
-      <div className='inputentry'>
-
-        <div>
-          <div className='emname'>
-
-            <div className='name'>
-              <label>Name</label>
-              <input type='text' onChange={(e) => { setName(e.target.value) }} ></input>
+              <div className='email'>
+                <label>Email</label>
+                <input type='email' name='from_mail' value={mail} onChange={(e) => { setMail(e.target.value); mailverify(e); }} />
+              </div>
             </div>
 
-            <div className='email'>
-              <label>Email</label>
-              <input type='email' onChange={(e) => mailverify(e)}></input>
+            <div className='warning'>
+              {testmail ? <h4>&nbsp;</h4> : <h4>Enter a valid email id</h4>}
             </div>
 
-
-          </div>
-
-          <div className='warning'>
-            {testmail ? <h4>&nbsp;</h4> : <h4>Enter a valid email id</h4>}
-          </div>
-
-          <div className='message'>
-            <label>Message</label>
-            <textarea style={{ resize: 'vertical' }} onChange={(e) => { setMessage(e.target.value) }}></textarea>
+            <div className='message'>
+              <label>Message</label>
+              <textarea name='message' style={{ resize: 'vertical' }} value={message} onChange={(e) => setMessage(e.target.value)} />
+            </div>
           </div>
         </div>
 
-      </div>
+        <div className='submit'>
+          <span>
 
-      <div className='submit'>
-        <span onClick={submitform}>Submit</span>
-      </div>
-
+          <button type='submit'>Submit</button>
+          </span>
+        </div>
+      </form>
       <div></div>
     </div>
   )
